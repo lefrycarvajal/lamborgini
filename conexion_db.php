ae81d06d51ccc70212b418881656ff1fea3aeafd
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2020-12-01 16:37:20 -0400

    Update conexion_db.php

--- conexion_db.php
+++ conexion_db.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("	de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","hjkiot0qcb6bqv27","hxv4sqjoz1dp7ve4","ugo5batayfqgsml4");
+$conex = mysqli_connect("de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","hjkiot0qcb6bqv27","hxv4sqjoz1dp7ve4","ugo5batayfqgsml4");
 
 ?>
