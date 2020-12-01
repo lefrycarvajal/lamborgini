2284edab2a05bbce731f46464afe79fb91732742
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2020-12-01 16:35:37 -0400

    Update conexion_db.php

--- conexion_db.php
+++ conexion_db.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","lamborghini_registro");
+$conex = mysqli_connect("	de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","hjkiot0qcb6bqv27","hxv4sqjoz1dp7ve4","ugo5batayfqgsml4");
 
 ?>
